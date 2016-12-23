578344c4fd24e302984bb0ed8640c01db4ea0333
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-12-22 23:23:14 -0600

    Front-end jQ todo list. Learned some neat stuff about event handlers.

--- section18/Todo-List-Project/assets/js/todos.js
+++ section18/Todo-List-Project/assets/js/todos.js
@@ -1,1 +1,19 @@
-alert("Connected!");
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+// click on trash to delete Todo
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13) {
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
